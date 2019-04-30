07b8e1cbdde5d74b9325dc47ccfda9345aa129c1
Amir Abu Hasan <amirabuhasan@dahmakan.com>
2019-05-01 00:20:10 +0800

    initial skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const tilesInRow = 4;
 
+class App extends Component {
+    state = {
+        dictionary: [],
+        boardRows: []
+    };
+
+    componentDidMount() {
+        fetch('data/dictionary.txt')
+            .then(response => response.text())
+            .then(text => this.setState({ dictionary: text.split("\n") }));
+
+        fetch('data/testBoard.txt')
+            .then(response => response.text())
+            .then(text => {
+                const tileArray = text.split(", ");
+                let boardRows = [];
+                while (tileArray.length > 0) {
+                    boardRows.push(tileArray.splice(0, tilesInRow));
+                }
+                this.setState({ boardRows })
+            })
+    }
+    render() {
+        const { dictionary, boardRows } = this.state;
+        const isLoading = dictionary.length === 0 || boardRows === 0;
+        return (
+            <div>
+               <div className='board'>
+                   { !isLoading
+                   &&
+                       (
+                           boardRows.map((row) => (
+                               <div className='row'>
+                                   { row.map(tile => (
+                                       <div>{ tile }</div>
+                                   ))}
+                               </div>
+                           ))
+                       )
+                   }
+               </div>
+            </div>
+        );
+    }
+}
 export default App;
