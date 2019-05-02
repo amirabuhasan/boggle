a59850f3cb9c3173fc0bff611319a6369144d877
Amir Abu Hasan <amirabuhasan@gmail.com>
2019-05-03 03:22:31 +0800

    Updates

---
+++ precache-manifest.3be90cbd7f968118c9004533ae6f5687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1818585d7f82dcdf7511fac48f170fa0",
+    "url": "/boggle/index.html"
+  },
+  {
+    "revision": "86d92e043d7f1f02edb4",
+    "url": "/boggle/static/css/main.0179af1e.chunk.css"
+  },
+  {
+    "revision": "99fcdcef069a071dcd05",
+    "url": "/boggle/static/js/2.f2cd8dd1.chunk.js"
+  },
+  {
+    "revision": "86d92e043d7f1f02edb4",
+    "url": "/boggle/static/js/main.97e3c91b.chunk.js"
+  },
+  {
+    "revision": "b8b6f2b6c7b529166c30",
+    "url": "/boggle/static/js/runtime~main.72847329.js"
+  }
+]);
